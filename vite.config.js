41ee03e0718f3a889182ba9bcf5b2f53bcee1f56
ritikaj23 <ritikaj@skillup.online>
2025-07-04 01:25:14 -0400

    modified requirements.txt

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-  import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-    export default defineConfig({
-      plugins: [react()],
-      server: {
-        proxy: {
-          '/api': {
-            target: 'https://ritikaj-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-            changeOrigin: true,
-            secure: false
-          }
-        }
-      }
-    });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
